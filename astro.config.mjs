6e490bef2d55c2113dd8882e477175130b94dc09
Ben <80292993+Benjino16@users.noreply.github.com>
2026-05-24 16:53:32 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ const isGithubPages = process.env.DEPLOY_TARGET === 'gh-pages';
 
 export default defineConfig({
   site: isGithubPages ? 'https://benjino16.github.io' : 'https://rundlinge.de',
-  base: isGithubPages ? '/rundlinge-website' : '/',
+  base: isGithubPages ? '/rundlinge-website/' : '/',
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
